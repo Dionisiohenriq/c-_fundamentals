972a84e01c125da3740264e55a322ef9f5903c0a
dionisiohenriq <henriquedionisio.dionisioferre@gmail.com>
2023-08-20 22:11:49 -0300

    Implementando as restrições de movimento

--- Composition/xadrez-console/Program.cs
+++ Composition/xadrez-console/Program.cs
@@ -7,24 +7,38 @@ try
     PartidaDeXadrez partida = new PartidaDeXadrez();
     while (!partida.Terminada)
     {
-        Console.Clear();
-        Tela.ImprimirTabuleiro(partida.tab);
+        try
+        {
+            Console.Clear();
+            Tela.ImprimirTabuleiro(partida.tab);
 
-        Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Turno: " + partida.Turno);
+            Console.WriteLine("Aguardando jogada: " + partida.JogadorAtual);
 
-        Console.Write("Origem: ");
-        Posicao origem = Tela.LerPosicaoXadrez().ToPosicao();
+            Console.Write("Origem: ");
+            Posicao origem = Tela.LerPosicaoXadrez().ToPosicao();
+            partida.ValidarPosicaoDeOrigem(origem);
 
-        bool[,] posicoesPossiveis = partida.tab.Peca(origem).MovimentosPossiveis();
 
-        Console.Clear();
-        Tela.ImprimirTabuleiro(partida.tab, posicoesPossiveis);
+            bool[,] posicoesPossiveis = partida.tab.Peca(origem).MovimentosPossiveis();
+
+            Console.Clear();
+            Tela.ImprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+            Console.WriteLine();
+            Console.Write("Destino: ");
+            Posicao destino = Tela.LerPosicaoXadrez().ToPosicao();
+            partida.ValidarPosicaoDeDestino(origem, destino);
+
+            partida.RealizaJogada(origem, destino);
+        }
+        catch(TabuleiroException e)
+        {
+            Console.WriteLine(e.Message);
+            Console.ReadLine();
+        }
         
-        Console.WriteLine();
-        Console.Write("Destino: ");
-        Posicao destino = Tela.LerPosicaoXadrez().ToPosicao();
-
-        partida.ExecutaMovimento(origem, destino);
     }
 
 }
--- Composition/xadrez-console/Tabuleiro/Peca.cs
+++ Composition/xadrez-console/Tabuleiro/Peca.cs
@@ -20,6 +20,27 @@
             QtdMovimentos++;
         }
 
+        public bool ExisteMovimentosPossiveis()
+        {
+            bool[,] mat = MovimentosPossiveis();
+            for (int i = 0; i < Tab.Linha;  i++)
+            {
+                for (int j = 0; j < Tab.Coluna; j++)
+                {
+                    if (mat[i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool PodeMoverPara(Posicao pos)
+        {
+            return MovimentosPossiveis()[pos.Linha, pos.Coluna];
+        }
+
         public abstract bool[,] MovimentosPossiveis();
     }
 }
