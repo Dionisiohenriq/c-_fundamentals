62d5737685f73591eea66c82b28f62d0ad68c2ba
Dionisiohenriq <henriquedionisio.dionisioferre@gmail.com>
2024-03-09 05:09:51 -0300

    ajustes

--- Composition/Get_Hash_Code_Equals/Entities/Client.cs
+++ Composition/Get_Hash_Code_Equals/Entities/Client.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Get_Hash_Code_Equals.Entities
+﻿namespace Get_Hash_Code_Equals.Entities
 {
     public class Client
     {
--- Composition/Get_Hash_Code_Equals/Program.cs
+++ Composition/Get_Hash_Code_Equals/Program.cs
@@ -4,6 +4,7 @@
 Client a = new Client{Name = "Henrique", Age = 35};
 Client b = new Client { Name = "Henrique", Age = 32 };
 
-Console.WriteLine(a.Equals(b));
+Console.WriteLine(a.Equals(b)); // Conteúdo igual = True 
+Console.WriteLine(a == b); // Referência de memória diferente = False
 Console.WriteLine(a.GetHashCode());
 Console.WriteLine(b.GetHashCode());
