19549f2733b0f5fca14771175e9ae1e4c69ff7cd
Dionisiohenriq <henriquedionisio.dionisioferre@gmail.com>
2024-03-09 06:18:53 -0300

    Hash_comparison

---
+++ Composition/Hash_Comparison/Program.cs
@@ -0,0 +1,25 @@
+﻿using Hash_Comparison.Entities;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        HashSet<string> set = new HashSet<string>();
+
+        set.Add("Maria");
+        set.Add("Alex");
+
+        // Primeiro executa GetHashCode e depois Equals para confirmar
+        Console.WriteLine(set.Contains("Maria"));
+
+        HashSet<Product> productA = [new Product("TV", 1.100), new Product("Notebook", 1.200)];
+        HashSet<Point> pointA = [new Point(3, 4), new Point(5, 10)];
+
+        Product prod = new Product("Notebook", 1.200);
+        Point point = new Point(5, 10);
+
+        Console.WriteLine(productA.Contains(prod));
+        Console.WriteLine(pointA.Contains(point));
+
+    }
+}
